2bd1d769f720d1c09242feeecef2684d6fc546bd
Rob Kitromilides <robkitro@gmail.com>
2015-08-14 23:06:01 +0100

    add names to users , names on pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,5 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type =>  /\Aimage\/.*\Z/
 
-
+validates :description, presence: true
+validates :image, presence: true
 end
